593498e3bfeac60ad0c2b1703222fd082334d170
AlinaSyvachenko <141357750+AlinaSyvachenko@users.noreply.github.com>
2024-03-21 16:45:40 +0200

    Revert "add 6 section rooms owlCarousel"
    This reverts commit f10c5160dd60ded73b778a23764c55e0833ba529.

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ document.getElementById('burger').addEventListener('click', function () {
 $(document).ready(function () {
   $('.owl-carousel').owlCarousel({
     items: 1,
-    nav: false, 
+    nav: false, // Вимкніть вбудовані стрілки
     loop: true,
     dots: false,
     margin: 30,
@@ -17,10 +17,10 @@ $(document).ready(function () {
     navText: [
       "<span class='custom-prev'>Previous</span>",
       "<span class='custom-next'>Next</span>",
-    ],
+    ], // Використовуйте власні стрілки
   });
 
-
+  // Прив'яжіть функції до кнопок вашої каруселі
   $('.custom-next').click(function () {
     $('.owl-carousel').trigger('next.owl.carousel');
   });
@@ -30,3 +30,26 @@ $(document).ready(function () {
 });
 
 
+// $('.owl-carousel').owlCarousel({
+//   items: 1,
+//   // loop: true,
+//   // margin: 30,
+//   // nav: true,
+//   // loop: false,
+//   loop: false,
+//   autoplay: true,
+//   autoplayTimeout: 10000,
+//   autoplayHoverPause: true,
+//   infinite: true,
+//   responsive: {
+//     1024: {
+//       items: 3,
+//     },
+//   },
+// });
+// $('.play').on('click', function () {
+//   owl.trigger('play.owl.autoplay', [10000]);
+// });
+// $('.stop').on('click', function () {
+//   owl.trigger('stop.owl.autoplay');
+// });
